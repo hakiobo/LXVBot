335993c7da281d187ec5ef1b2eb5de94e0892ab0
Huebner <Hakiobo7@gmail.com>
2021-03-03 16:06:16 -0600

    added ping command

---
+++ src/main/kotlin/commands/Ping.kt
@@ -0,0 +1,40 @@
+package commands
+
+import LXVBot
+import commands.utils.BotCommand
+import commands.utils.CommandUsage
+import dev.kord.core.behavior.edit
+import dev.kord.core.behavior.reply
+import dev.kord.core.event.message.MessageCreateEvent
+import toInstant
+import java.time.Duration
+import java.time.Instant
+
+object Ping : BotCommand {
+    override val name: String
+        get() = "ping"
+    override val description: String
+        get() = "Pong!"
+    override val aliases: List<String>
+        get() = listOf("pong")
+    override val usages: List<CommandUsage>
+        get() = listOf(CommandUsage(listOf(), "Ping ${LXVBot.BOT_NAME}!"))
+
+    override suspend fun LXVBot.cmd(mCE: MessageCreateEvent, args: List<String>) {
+        val received = Instant.now()
+        val msg = "\ud83c\udfd3 Pong! Received in ${Duration.between(mCE.message.id.toInstant(), received).toMillis()} ms"
+        val sent = mCE.message.reply {
+            content = "$msg\nReply Sent In `Waiting . . .`"
+            this.allowedMentions {
+                repliedUser = false
+            }
+        }
+        val time = Duration.between(received, sent.id.toInstant()).toMillis()
+        sent.edit {
+            this.content = "$msg\nReply Sent In $time ms"
+            this.allowedMentions {
+                repliedUser = false
+            }
+        }
+    }
+}
